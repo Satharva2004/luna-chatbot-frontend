f753849f81fcdd6d745390dc479791b33059e43c
Satharva2004 <satharva2004@gmail.com>
2025-09-17 15:38:29 +0530

    finally

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -36,7 +36,7 @@ export function LoginForm({
     setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://assignment-backend-one-khaki.vercel.app/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/components/ui/signup-form.tsx
+++ src/components/ui/signup-form.tsx
@@ -37,7 +37,7 @@ export function SignupForm({
     setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/register', {
+      const response = await fetch('https://assignment-backend-one-khaki.vercel.app/api/users/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
